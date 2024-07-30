403215a78d9ea14ad3d0d97a79a934f64c39f913
Marcos Sasaki <sasakimarcos95@gmail.com>
2024-07-30 10:32:56 -0300

    working collatz

--- haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,9 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz = error "You need to implement this function."
+collatz n = if n <= 0 then Nothing else collatzRecursive n 0
+  where
+    collatzRecursive num count
+      | num == 1 = Just count
+      | num `mod` 2 == 0 = collatzRecursive (num `div` 2) (count + 1)
+      | otherwise = collatzRecursive (3 * num + 1) (count + 1)
