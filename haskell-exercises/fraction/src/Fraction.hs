67e39715c825699093fc0306373a1ab5226b0fba
Marcos Sasaki <sasakimarcos95@gmail.com>
2024-07-29 23:28:57 -0300

    fraction working implementation

--- haskell-exercises/fraction/src/Fraction.hs
+++ haskell-exercises/fraction/src/Fraction.hs
@@ -4,28 +4,65 @@ type Fraction = (Int, Int)
 
 -- Implement the `add` Function
 
+numerator :: Fraction -> Int
+numerator (n, _) = n
+
+denominator :: Fraction -> Int
+denominator (_, d) = d
+
 add :: Fraction -> Fraction -> Fraction
-add n d = error "Implement it"
+add n d = (num, den)
+  where
+    n1 = numerator n
+    n2 = numerator d
+    d1 = denominator n
+    d2 = denominator d
+    num = n1 * d2 + n2 * d1
+    den = d1 * d2
 
 -- Implement the `sub` Function
 
 sub :: Fraction -> Fraction -> Fraction
-sub n d = error "Implement it"
+sub n d = (num, den)
+  where
+    n1 = numerator n
+    n2 = numerator d
+    d1 = denominator n
+    d2 = denominator d
+    num = n1 * d2 - n2 * d1
+    den = d1 * d2
 
 -- Implement the `mul` Function
 
 mul :: Fraction -> Fraction -> Fraction
-mul n d = error "Implement it"
+mul n d = (num, den)
+  where
+    n1 = numerator n
+    n2 = numerator d
+    d1 = denominator n
+    d2 = denominator d
+    num = n1 * n2
+    den = d1 * d2
 
 -- Implement the `divide` Function
 
 divide :: Fraction -> Fraction -> Fraction
-divide n d = error "Implement it"
+divide n d = (num, den)
+  where
+    n1 = numerator n
+    n2 = numerator d
+    d1 = denominator n
+    d2 = denominator d
+    num = n1 * d2
+    den = n2 * d1
 
 -- Implement the `hcf` Function
 
 hcf :: Int -> Int -> Int
-hcf n d = error "Implement it"
-
+hcf n d
+  | n == 0 = d
+  | d == 0 = n
+  | n > d = hcf (n - d) d
+  | otherwise = hcf n (d - n) 
 
     
