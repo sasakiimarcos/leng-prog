3fd2ab7a413c54dbd4e99a71e6e05d69463fdb6e
Marcos Sasaki <sasakimarcos95@gmail.com>
2024-08-11 11:06:03 -0300

    lists

--- haskell-exercises/lists/src/Lists.hs
+++ haskell-exercises/lists/src/Lists.hs
@@ -1,7 +1,7 @@
 module Lists (member, union, intersection, difference,
               insert, insertionSort,
               binaryToDecimal, toDecimal, toDec, decimal,
-              binaryAdd) where
+              binaryAdd, firsts) where
   
 import Data.Char(digitToInt)  
 
@@ -16,39 +16,88 @@ union (x:xs) ys
   | member x ys = union xs ys
   | otherwise   = x : union xs ys
 
+invert:: [a] -> [a]
+invert [] = []
+invert (x:xs) = invert xs ++ [x]
+
 -- Remove Implementations, from, here on
 
 intersection:: [Int] -> [Int] -> [Int]
-intersection = error "Implement it"
+intersection [] _ = []
+intersection (x:xs) ys
+  | member x ys = x : intersection xs ys
+  | otherwise = intersection xs ys
 
 difference:: [Int] -> [Int] -> [Int]
-difference  = error "Implement it"
+difference [] _ = []
+difference (x:xs) ys
+  | member x ys = difference xs ys
+  | otherwise = x : difference xs ys
 
 insert:: Int -> [Int] -> [Int]
-insert = error "Implement it"
+insert i xs
+  | xs == [] = i : []
+  | i <= head xs = i : xs
+  | otherwise = head xs : insert i (tail xs)
 
 insertionSort :: [Int] -> [Int]
-insertionSort = error "Implement it"
+insertionSort xs
+  | xs == [] = []
+  | otherwise = insert (head xs) (insertionSort (tail xs))
+  
+-- Implementation using foldr
+-- insertionSort xs = foldr insert [] xs
 
 binaryToDecimal :: [Int] -> Int
-binaryToDecimal = error "Implement it"
+binaryToDecimal [] = 0
+binaryToDecimal (x:xs) = (x * 2 ^ (length xs)) + binaryToDecimal xs
     
 toDecimal :: Int -> [Int] -> Int
-toDecimal = error "Implement it"
+toDecimal _ [] = 0
+toDecimal a (x:xs) = (x * a ^ (length xs)) + toDecimal a xs 
     
 toDec::Int -> String -> Int
-toDec base s =  = error "Implement it"
+toDec base s = toDecimal base (map digitToInt s)
 
 -- Same as `toDec` But use a list comprehension
 
 decimal::Int -> String -> Int
-decimal  = error "Implement it"
+decimal base s = sum [(digitToInt x) * base ^ ((length s) - 1 - i) | (i, x) <- zip [0..] s]
+--  where
+--    sum::[Int] -> Int
+--    sum [] = 0
+--    sum (x:xs) = x + sum xs
+-- another way of implementing this is by creating a helper function to invert the list, thus avoiding the need to calculate the length of the list
 
 firsts::[a] -> [[a]]
-firsts = error "Implement it"
+firsts [] = []
+firsts (x:xs) = [x] : firstsRecursive xs [x]
+  where
+    firstsRecursive:: [a] -> [a] -> [[a]]
+    firstsRecursive [] _ = []
+    firstsRecursive (y:ys) zs = (zs++[y]) : firstsRecursive ys (zs ++ [y])
 
 -- Given two String that represents numbers in binary implement the 'binaryAdd' function
 -- DO NOT USE a predefined '+' operation
 
 binaryAdd::String -> String -> String
-binaryAdd  = error "Implement it"
+binaryAdd [] [] = "0"
+binaryAdd n1 n2 = invert (binaryAddRecursive (invert n1) (invert n2) 0)
+
+binaryAddRecursive:: String -> String -> Int -> String
+binaryAddRecursive "" "" c = if c == 0 then "" else "1"
+binaryAddRecursive "" (y:ys) c = (fst (binaryDigitAdd '0' y c)) : binaryAddRecursive [] ys (snd (binaryDigitAdd '0' y c))
+binaryAddRecursive (x:xs) "" c = (fst (binaryDigitAdd x '0' c)) : binaryAddRecursive xs [] (snd (binaryDigitAdd x '0' c))
+binaryAddRecursive (x:xs) (y:ys) c = (fst (binaryDigitAdd x y c)) : binaryAddRecursive xs ys (snd (binaryDigitAdd x y c))
+
+binaryDigitAdd:: Char -> Char -> Int -> (Char, Int)
+binaryDigitAdd c1 c2 i
+  | c1 == '0' && c2 == '0' && i == 0 = ('0', 0)
+  | c1 == '0' && c2 == '0' && i == 1 = ('1', 0)
+  | c1 == '0' && c2 == '1' && i == 0 = ('1', 0)
+  | c1 == '0' && c2 == '1' && i == 1 = ('0', 1)
+  | c1 == '1' && c2 == '0' && i == 0 = ('1', 0)
+  | c1 == '1' && c2 == '0' && i == 1 = ('0', 1)
+  | c1 == '1' && c2 == '1' && i == 0 = ('0', 1)
+  | c1 == '1' && c2 == '1' && i == 1 = ('1', 1)
+  | otherwise = ('0', 0)
