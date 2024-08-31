fa0ae20c63946924cd26132b608ef0963333779f
Marcos Sasaki <sasakimarcos95@gmail.com>
2024-08-30 22:00:12 -0300

    frequencies

--- haskell-exercises/frequencies/src/Frequencies.hs
+++ haskell-exercises/frequencies/src/Frequencies.hs
@@ -7,13 +7,21 @@ import Data.Tuple(swap)
 type Frequency = (Int, Char)
 
 frequencies::String -> [Frequency]
-frequencies = error "Implement it"
+frequencies s =  insertionSort [ (i, c) | (c, i) <- (Map.toList $ frequencyMap s) ]
 
 frequencyMap::(Ord a) => [a] -> Map a Int
-frequencyMap = error "Implement it"
+frequencyMap ls = _frequencyMap ls Map.empty
+
+_frequencyMap::(Ord a) => [a] -> Map a Int -> Map a Int
+_frequencyMap [] m = m
+_frequencyMap (x:xs) m = _frequencyMap xs (Map.insertWith (+) x 1 m)
 
 insert::(Ord a) => a -> [a] -> [a]
-insert = error "Implement it"
+insert a [] = [a]
+insert a (x:xs)
+  | a <= x = a : x : xs
+  | otherwise = x : (insert a xs)
 
 insertionSort :: (Ord a) => [a] -> [a]
-insertionSort = error "Implement it"
+insertionSort [] = []
+insertionSort ls = foldr insert [] ls
--- haskell-exercises/frequencies/src/Trie.hs
+++ haskell-exercises/frequencies/src/Trie.hs
@@ -1,23 +1,35 @@
 module Trie  (Trie(..), left, right, find, decode, toList) where
 
 import Bit
-  
-data Trie a = Leaf a
-            | Trie a :-: Trie a deriving (Eq, Show, Ord)
 
--- Bring definition from previous TP
+data Trie a = Nil | Leaf a | (Trie a) :-: (Trie a) deriving (Show, Eq)
 
 left::Trie a -> Trie a
-left = error "Define it"
+left Nil = error "Empty Node"
+left (Leaf _) = error "Node is a Leaf"
+left (l :-: _) = l
 
 right::Trie a -> Trie a
-left = error "Define it"
+right Nil = error "Empty Node"
+right (Leaf _) = error "Node is a Leaf"
+right (_ :-: r) = r
   
 find::Bits -> Trie a -> a
-find = error "Define it"
+find _ (Leaf x) = x
+find (x1 : xs) t = if x1 == T then find xs (right t) else find xs (left t)
 
 decode::Bits -> Trie Char -> String
-decode = error "Define it"
+decode bs t = decodeRecursive bs t t
 
+decodeRecursive:: Bits -> Trie Char -> Trie Char -> String
+decodeRecursive _ Nil _ = error "Leaf not found"
+decodeRecursive [] (Leaf c) root = c : ""
+decodeRecursive ls (Leaf c) root = c : decodeRecursive ls root root
+decodeRecursive (x:xs) t root = if x == T then (decodeRecursive xs (right t) root) else (decodeRecursive xs (left t) root)
+  
 toList::Trie a -> [(a, Bits)]
-toList = error "Define it"            
+toList t = toListRecursive t []
+  
+toListRecursive:: Trie a -> Bits -> [(a, Bits)]
+toListRecursive (Leaf c) bs = [(c, bs)]
+toListRecursive (l :-: r) bs = toListRecursive l (bs++[F]) ++ toListRecursive r (bs++[T])     
